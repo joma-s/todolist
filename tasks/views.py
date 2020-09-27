44e065b4113cc5616a1e66ed2642c5dcf7cf141e
João Marcos Soares <joaomarcossoares@Joaos-MacBook-Pro.local>
2020-09-27 01:35:31 -0300

    remove fixed tasks list and create it per session

--- tasks/views.py
+++ tasks/views.py
@@ -3,30 +3,30 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-tasks = []
-
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
 # Create your views here.
 
 def index(request):
-    return render(request, "tasks/index.html", {
-        "tasks": tasks
+    if "tasks" not in request.session: # if there's no tasks list in the session, create a blank one
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {  # return the index file with the session's tasks list
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
-    if request.method == "POST":
-            form = NewTaskForm(request.POST)
-            if form.is_valid():
-                task = form.cleaned_data["task"]
-                tasks.append(task)
-                return HttpResponseRedirect(reverse("tasks:index"))
+    if request.method == "POST":        # if something has been sent in post
+            form = NewTaskForm(request.POST)    # save user data to the form variable
+            if form.is_valid():                 # if data is valid
+                task = form.cleaned_data["task"]    # save cleaned data in the task variable
+                request.session["tasks"] += [task] # append task to the session's tasks list
+                return HttpResponseRedirect(reverse("tasks:index"))     # and redirect to the index page
             else:
-                return render(request, "tasks/add.html", {
+                return render(request, "tasks/add.html", {      # otherwise, return the add page with the form filled out with user data so the user can see any errors from the server
                     "form" : form
                 })
 
-    return render(request,"tasks/add.html", {
+    return render(request,"tasks/add.html", {    # if no post request has been sent, display empty form
         "form" : NewTaskForm()
     })
